a747c898bce10c43c4e99d0eee7ef62bf94817d5
James Ford <james.f@mmtdigital.co.uk>
2014-01-01 14:44:12 +0000

    Add LGPL license information

--- lzw.js
+++ lzw.js
@@ -1,3 +1,29 @@
+/**
+ * @license A LZW encoding/decoding library for JavaScript, heavily borrowed
+ * from several open-source code examples, and reformatted into a library
+ * similar to how the JSON library implementations work.
+ *
+ * On GitHub: https://github.com/psyked/LZW-js
+ *
+ * Copyright (C) 2014 James Ford <jford@psyked.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ *
+ * @type {LZW|*|{encode: encode, decode: decode}}
+ */
 var LZW = window.LZW || {
     encode: function encode(s) {
         var dict = {};
